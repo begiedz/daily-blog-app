9c12bf60247498429af9b438a87d3ae56444c2c4
Justyna Małek <malekju_aehit@students.vizja.pl>
2025-03-09 19:05:42 +0100

    utworzenie środowiska

---
+++ Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace daily_blog_app.Models
+{
+    public class User {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
