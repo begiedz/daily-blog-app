3c641e6b4c1cabbf69ac211a1562b96c8b69ae95
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2025-03-13 12:39:26 +0100

    feat: add navigation, layout, menu

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,32 +2,21 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Link
 } from "react-router-dom";
 
-import Start from './pages/Start'
-import About from './pages/About'
+import AppLayout from "./layouts/AppLayout";
+import appRoutes from "./AppRoutes";
 
 export default function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/start">Start</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
-
+      <AppLayout>
         <Routes>
-          <Route path="/start" element={<Start />} />
-          <Route path="/about" element={<About />} />
+          {appRoutes.map((route, index) => (
+            <Route key={index} path={route.path} element={route.pageElement} />
+          ))}
         </Routes>
-      </div>
+      </AppLayout>
     </Router>
   )
 }
