566814e53d75ed9cac1966d96985aadc77c4fd85
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2025-03-15 11:42:19 +0100

    chore(layout): add footer, separate header

---
+++ client/src/components/layout/Header.tsx
@@ -0,0 +1,30 @@
+const MenuButton = () => {
+  return (
+    <label htmlFor="drawer" aria-label="open sidebar" className="btn btn-square btn-ghost">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        className="inline-block h-6 w-6 stroke-current">
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M4 6h16M4 12h16M4 18h16"></path>
+      </svg>
+    </label>
+  )
+}
+
+export default function Header() {
+  return (
+    <header className="navbar bg-base-300 w-full lg:hidden">
+      <div className="flex-none lg:hidden">
+        <MenuButton />
+      </div>
+      <div className="hidden flex-none lg:block">
+
+      </div>
+    </header>
+  )
+}
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,17 @@
 const Home = () => {
     return (
         <>
+            <h2>Latest Posts:</h2>
+            <ul>
+                <li>elo1</li>
+                <li>elo2</li>
+                <li>elo3</li>
+                <li>elo4</li>
+                <li>elo5</li>
+                <li>elo6</li>
+                <li>elo7</li>
+                <li>elo8</li>
+            </ul>
         </>
     )
 }
