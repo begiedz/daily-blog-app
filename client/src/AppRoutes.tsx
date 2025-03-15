07d2adc6d01513d26a6ba201e4321bfc6bbd4926
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2025-03-15 14:17:44 +0100

    feat: add create post page

--- client/src/AppRoutes.tsx
+++ client/src/AppRoutes.tsx
@@ -1,16 +1,25 @@
 import About from './pages/About'
 import Home from './pages/Home'
+import Create from './pages/Create'
 
 const appRoutes = [
   {
     value: 'Home',
     path: '/',
-    pageElement: <Home />
+    pageElement: <Home />,
+    private: false
   },
   {
     value: 'About',
     path: '/about',
-    pageElement: <About />
-  }
+    pageElement: <About />,
+    private: false
+  },
+  {
+    value: 'New Post',
+    path: '/create',
+    pageElement: <Create />,
+    private: true
+  },
 ]
 export default appRoutes;
---
+++ client/src/pages/Create.tsx
@@ -0,0 +1,26 @@
+const Create = () => {
+  return (
+    <>
+      <h2 className="text-3xl font-bold mb-4">Create New Post</h2>
+      <form className="fieldset w-md bg-base-200 border border-base-300 p-8 rounded-box">
+
+        <label className="fieldset-label">Title</label>
+        <input type="text" className="input" placeholder="My post" />
+
+        <label className="fieldset-label">Subject</label>
+        <input type="text" className="input" placeholder="My post is awesome!" />
+
+        <label className="fieldset-label">Cover Image</label>
+        <input type="file" className="file-input" />
+        <label className="fieldset-label opacity-50">Max size 2MB</label>
+
+        <label className="fieldset-label">Text</label>
+        <textarea className="input text-wrap w-full h-40 p-2" placeholder="Today I wanted to tell you how awesome my new post is..."></textarea>
+
+        <input className="btn bg-base-content dark:bg-base-content/80 text-base-100" type="submit" value="Send" />
+      </form>
+    </>
+  )
+}
+
+export default Create
