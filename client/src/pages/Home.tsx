c76bc345eb950dce6b48c718e54326d384909d7c
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2025-03-18 11:35:28 +0100

    feat add tanstack store and axios

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,63 +1,53 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import { Link } from "react-router-dom"
-import { IPost } from "../types"
+
+import { useStore } from "@tanstack/react-store"
+import { postsStore } from "../store"
+
+import axios from "axios"
 
 const Home = () => {
-
-  const [posts, setPosts] = useState<IPost[] | []>([])
-
-  const fetchData = async (url: string) => {
-    try {
-      const response = await fetch(url)
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`)
-      }
-
-      const json = await response.json()
-      console.log(json)
-      return json
-
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        console.error(error.message)
-      } else {
-        console.error('unknown error ocured');
-      }
-    }
-  }
+  const posts = useStore(postsStore)
 
   useEffect(() => {
-    const getPosts = async (url: string) => {
-      const data = await fetchData(url)
-      if (data) setPosts(data)
+    const getPosts = async () => {
+      try {
+        const response = await axios.get('/test-api/posts.json')
+        postsStore.setState(() => response.data)
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching posts:", error instanceof Error ? error.message : "Unknown error")
+      }
     }
-    getPosts('/test-api/posts.json')
+
+    getPosts()
   }, [])
 
   return (
     <>
       <h2 className="text-3xl font-bold mb-4">Latest Posts:</h2>
       <ul className="list bg-base-100 rounded-box shadow-md">
-        {posts.map((post) => {
-          return (
-            <li key={post.id}>
-              <Link to="/post" className="list-row">
-                <div>
-                  <img
-                    className="size-10 rounded-box"
-                    src={post.img} />
-                </div>
-                <div>
-                  <h3 className="text-xl">{post.title}</h3>
-                  <div className="text-xs uppercase font-semibold opacity-60">{post.subject}</div>
-                </div>
-                <p className="list-col-wrap text-xs truncate">
-                  {post.snippet}
-                </p>
-              </Link>
-            </li>
-          )
-        })}
+        {posts.length > 0 &&
+          posts.map((post) => {
+            return (
+              <li key={post.id}>
+                <Link to="/post" className="list-row">
+                  <div>
+                    <img
+                      className="size-10 rounded-box"
+                      src={post.img} />
+                  </div>
+                  <div>
+                    <h3 className="text-xl">{post.title}</h3>
+                    <div className="text-xs uppercase font-semibold opacity-60">{post.subject}</div>
+                  </div>
+                  <p className="list-col-wrap text-xs truncate">
+                    {post.snippet}
+                  </p>
+                </Link>
+              </li>
+            )
+          })}
       </ul >
     </>
   )
