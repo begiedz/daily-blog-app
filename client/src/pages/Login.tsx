ba505c9a0aa3785e68110d12763d2895920cf622
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2025-03-27 18:12:45 +0100

    feat: add register form

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -4,13 +4,15 @@ import { loginRequest } from '../api/authApi'
 import { loginUser } from '../store/authStore'
 
 const Login = () => {
+  const [isRegister, setIsRegister] = useState(false)
+  const [error, setError] = useState('')
   const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
-  const [error, setError] = useState('')
   const navigate = useNavigate()
 
-  const handleSubmit = async (event: React.FormEvent) => {
+  const handleLogin = async (event: React.FormEvent) => {
     event.preventDefault()
     try {
       const { token, user } = await loginRequest(username, password)
@@ -21,14 +23,32 @@ const Login = () => {
     }
   }
 
+  const handleRegister = async (event: React.FormEvent) => {
+    event.preventDefault()
+  }
+
   return (
     <>
-      <h2 className="text-3xl font-bold mb-4">Log in</h2>
+      <h2 className="text-3xl font-bold mb-4">{isRegister ? 'Register' : 'Log in'}</h2>
       {error && <p className="text-red-500">{error}</p>}
       <form
-        onSubmit={handleSubmit}
+        onSubmit={isRegister ? handleRegister : handleLogin}
         className="fieldset w-md max-w-full bg-base-200 border border-base-300 p-8 rounded-box"
       >
+        {isRegister && (
+          <div>
+            <label className="fieldset-label">E-mail</label>
+            <input
+              type="email"
+              placeholder="admin@admin.blog"
+              required
+              value={email}
+              onChange={event => setEmail(event.target.value)}
+              className="input validator w-full"
+            />
+            <div className="validator-hint">Enter valid email</div>
+          </div>
+        )}
         <div>
           <label className="fieldset-label">Username</label>
           <input
@@ -47,20 +67,28 @@ const Login = () => {
             type="password"
             placeholder="Password"
             required
-            value={username}
+            value={password}
             onChange={event => setPassword(event.target.value)}
             className="input validator w-full"
           />
           <div className="validator-hint">Enter valid password</div>
         </div>
-
         <button
           type="submit"
           className="btn btn-primary"
         >
-          Login
+          {isRegister ? 'Register' : 'Log in'}
         </button>
       </form>
+      <p>
+        {(isRegister ? "Don't have an account?" : 'Already have an account?') + ' '}
+        <button
+          onClick={() => setIsRegister(!isRegister)}
+          className="link"
+        >
+          {isRegister ? 'Log in' : 'Register'}
+        </button>
+      </p>
     </>
   )
 }
