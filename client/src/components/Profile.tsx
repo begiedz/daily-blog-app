29ee9d3938a185eaf89a940566a668becd48b8e3
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2025-03-29 21:56:38 +0100

    feat: logout

--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -3,14 +3,29 @@ import { authStore } from '../store/authStore'
 const Profile = () => {
   const { user } = authStore.state
 
+  const handleLogout = () => {
+    authStore.setState(prevState => ({
+      ...prevState,
+      user: null,
+      isAuthenticated: false,
+    }))
+    localStorage.removeItem('token')
+    window.location.reload()
+  }
+
   return (
     <div>
       {user ? (
-        <div>
+        <div className="flex items-center justify-between gap-4">
           <p>
             Logged in as <span className="font-bold">{user.username}</span>
           </p>
-          <button className="btn"></button>
+          <button
+            onClick={handleLogout}
+            className="btn bg-base-content text-base-100"
+          >
+            Log out
+          </button>
         </div>
       ) : (
         <p>Not logged in</p>
