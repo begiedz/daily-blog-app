63d40ba89d3e9020223f7f2ac74640e2d402ab8f
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2025-03-29 16:27:33 +0100

    feat: register

--- client/src/api/authApi.ts
+++ client/src/api/authApi.ts
@@ -12,3 +12,16 @@ export const loginRequest = async (username: string, password: string) => {
     throw new Error(`Error logging in: ${error instanceof Error ? error.message : 'Unknown error'}`)
   }
 }
+export const registerRequest = async (email: string, username: string, password: string) => {
+  try {
+    const response = await axios.post('http://localhost:5017/api/Auth/register', {
+      username,
+      email,
+      password,
+    })
+    console.log('Response from API:', response)
+    return response.data
+  } catch (error) {
+    throw new Error(`Error logging in: ${error instanceof Error ? error.message : 'Unknown error'}`)
+  }
+}
