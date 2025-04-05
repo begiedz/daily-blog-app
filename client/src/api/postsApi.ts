3ae2ad5b75125901f45c080f74239eef827fef34
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2025-04-05 19:24:46 +0200

    feature: posts api connection

--- client/src/api/postsApi.ts
+++ client/src/api/postsApi.ts
@@ -3,9 +3,11 @@ import { postsStore } from '../store/postStore';
 
 export const getPosts = async () => {
   try {
-    const response = await axios.get('/test-api/posts.json');
+    const response = await axios.get(
+      'http://localhost:5017/api/Blog/all-posts',
+    );
     postsStore.setState(() => response.data);
-    console.log(response.data);
+    console.log('getPosts response: ', response.data);
   } catch (error) {
     console.error(
       'Error fetching posts:',
