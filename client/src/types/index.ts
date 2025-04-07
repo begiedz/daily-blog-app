a196fc8a53c81f566b3449354ded2c47e7afe8e6
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2025-04-08 00:07:47 +0200

    chore: fix IUser naming

---
+++ client/src/types/index.ts
@@ -0,0 +1,18 @@
+export interface IPost {
+  id: number;
+  slug: string;
+  title: string;
+  imgUrl: string;
+  createdAt: string;
+  content?: string;
+  excerpt?: string;
+  tags: string[];
+  author: string;
+}
+
+export interface IUser {
+  id: number;
+  name: string;
+  email: string;
+  role: string;
+}
