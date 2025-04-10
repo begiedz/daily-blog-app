48ea97ae0b254062c18fdb5cc4e3550d6afa0a3e
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2025-04-10 21:29:00 +0200

    feat: error handling++

--- client/src/api/utils/index.ts
+++ client/src/api/utils/index.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
-export const handleApiError = (error: unknown): never => {
-  if (axios.isAxiosError(error) && error.response?.data?.message) {
-    throw new Error(error.response.data.message);
+export const handleApiError = (err: unknown): never => {
+  if (axios.isAxiosError(err) && err.response?.data?.message) {
+    console.error(err);
+    throw new Error(err.response.data.message);
   }
   throw new Error('An unexpected error occurred.');
 };
