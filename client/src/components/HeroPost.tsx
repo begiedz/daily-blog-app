68aa3b2ba1070e489054fd4454f8520e3a0f1d9f
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2025-04-13 11:06:23 +0200

    chore: adjust post cards layout

--- client/src/components/HeroPost.tsx
+++ client/src/components/HeroPost.tsx
@@ -9,23 +9,23 @@ const HeroPost = ({
   tags,
 }: IPost) => {
   return (
-    <div className="card relative z-0 h-90 overflow-hidden shadow-sm">
+    <div className="card relative h-90 overflow-hidden shadow-sm">
       <img
         src={imageUrl || '/no-image.jpg'}
         alt={`${title || 'Post'} image`}
         className="absolute h-full w-full object-cover opacity-40"
       />
-      <div className="card-body absolute bottom-0 z-10 h-1/2 w-full">
+      <div className="card-body absolute bottom-0 w-full">
         <p className="font-bold">
           {author} • {new Date(createdAt).toLocaleDateString()}
         </p>
         <h3 className="card-title text-xl">{title}</h3>
         <p>{excerpt}</p>
-        <div className="space-x-2">
+        <div className="inline-flex flex-wrap gap-2">
           {tags.map((tag, index) => (
             <span
               key={index}
-              className="badge badge-outline badge-white rounded-full"
+              className="badge badge-outline badge-white gap-2 rounded-full"
             >
               {tag}
             </span>
