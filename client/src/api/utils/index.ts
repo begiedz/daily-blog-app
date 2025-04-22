9fde16c8e61045879a0cfaaa36630ac8d11c833d
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2025-04-22 23:48:55 +0200

    refactor: Api handling

--- client/src/api/utils/index.ts
+++ client/src/api/utils/index.ts
@@ -1,35 +1,28 @@
-import axios from 'axios';
-import { IApiError } from '../../types';
-import { setErrorState } from '../../store/errorStore';
+import { AxiosError, AxiosResponse } from 'axios';
+import { setNotification } from '../../store/notificationStore';
 
-export const handleApiError = (err: unknown): never => {
-  if (axios.isAxiosError(err) && err.response) {
-    const { status, data } = err.response;
-    console.error(status, data);
-    throw {
-      status,
-      message: data?.message || 'An unexpected error occurred.',
-    };
-  }
-  throw {
-    status: 500,
-    message: 'No connection to server. Please try again later.',
-  };
+interface ApiResponse {
+  message?: string;
+}
+
+export const handleApiSuccess = (res: AxiosResponse) => {
+  const status = res?.status || 200;
+  const message = res?.data?.message || 'Success';
+  setNotification({
+    status,
+    message,
+    type: 'success',
+  });
 };
 
-const isApiError = (err: unknown): err is IApiError => {
-  return (
-    typeof err === 'object' &&
-    err !== null &&
-    'status' in err &&
-    'message' in err &&
-    typeof (err as IApiError).status === 'number' &&
-    typeof (err as IApiError).message === 'string'
-  );
-};
+export const handleApiError = (err: AxiosError) => {
+  const data = err.response?.data as ApiResponse;
+  const status = err.response?.status || 500;
+  const message = data?.message || err.message || 'Unexpected error occurred.';
 
-export const setApiError = (err: unknown) => {
-  if (isApiError(err)) {
-    setErrorState(err);
-  }
+  setNotification({
+    status,
+    message,
+    type: 'error',
+  });
 };
